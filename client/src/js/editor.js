05dd203b4f0af68a96672b3a660393c173c556fb
djamiranda <djamiranda@gmail.com>
2023-09-12 20:23:25 -0400

    updated editor.js

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -1,16 +1,21 @@
 // Import methods to save and get data from the indexedDB database in './database.js'
 import { getDb, putDb } from './database';
+
+// Import the 'header' from './header.js' (assuming it contains some default content)
 import { header } from './header';
 
+// Create a class
 export default class {
   constructor() {
+    // Get content from localStorage
     const localData = localStorage.getItem('content');
 
-    // check if CodeMirror is loaded
+    // Check if CodeMirror is loaded
     if (typeof CodeMirror === 'undefined') {
       throw new Error('CodeMirror is not loaded');
     }
 
+    // Initialize CodeMirror editor
     this.editor = CodeMirror(document.querySelector('#main'), {
       value: '',
       mode: 'javascript',
@@ -22,20 +27,23 @@ export default class {
       tabSize: 2,
     });
 
-    // When the editor is ready, set the value to whatever is stored in indexeddb.
-    // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
+    // When the editor is ready, load data from IndexedDB
+    // Fall back to localStorage if nothing is stored in IndexedDB, and if neither is available, set the value to 'header'.
     getDb().then((data) => {
       console.info('Loaded data from IndexedDB, injecting into editor');
       this.editor.setValue(data || localData || header);
     });
 
+    // Event handler for editor change
     this.editor.on('change', () => {
+      // Save the editor's content to localStorage whenever it changes
       localStorage.setItem('content', this.editor.getValue());
     });
 
-    // Save the content of the editor when the editor itself is loses focus
+    // Event handler for when the editor loses focus
     this.editor.on('blur', () => {
       console.log('The editor has lost focus');
+      // Save the content to the IndexedDB database and localStorage
       putDb(localStorage.getItem('content'));
     });
   }
